df740fd8e2705e9ba67a27f2ae36d71f34ade2be
jayram <jaybabu706146@gmail.com>
2025-09-08 17:10:07 +0530

    add Profile Photo

--- My_Site/My_Profile/settings.py
+++ My_Site/My_Profile/settings.py
@@ -122,4 +122,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
